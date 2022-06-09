0409133ba26282cd87e105716c3a6959a5af9cf1
Yukami <46356129+Skiftstar@users.noreply.github.com>
2022-06-09 14:47:09 +0200

    Added ability to add tabCompleter

--- src/main/java/Kyu/SCommand.java
+++ src/main/java/Kyu/SCommand.java
@@ -4,15 +4,21 @@ import Kyu.LangSupport.LanguageHelper;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
-public final class SCommand implements CommandExecutor {
+public final class SCommand implements CommandExecutor, TabCompleter {
 
     private Consumer<CommandEvent> consumer = null;
+    private Function<TabEvent, List<String>> tabConsumer = null;
     private String execPerm = null;
     private int minArgs = 0;
     private boolean playerOnly = false;
@@ -43,6 +49,10 @@ public final class SCommand implements CommandExecutor {
         consumer = con;
     }
 
+    public void tabComplete(Function<TabEvent, List<String>> con) {
+        tabConsumer = con;
+    }
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         CommandEvent e = new CommandEvent(sender, command, label, args);
@@ -67,6 +77,12 @@ public final class SCommand implements CommandExecutor {
             consumer.accept(e);
             return true;
         } else return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd,
+            @NotNull String label, @NotNull String[] args) {
+        return tabConsumer != null ? tabConsumer.apply(new TabEvent(sender, cmd, label, args)) : Collections.emptyList();
     }
 
     public class CommandEvent {
@@ -106,6 +122,38 @@ public final class SCommand implements CommandExecutor {
             return sender instanceof Player ? (Player) sender : null;
         }
 
+
+    }
+
+    public class TabEvent {
+
+        private CommandSender sender;
+        private Command command;
+        private String label;
+        private String[] args;
+
+        public TabEvent(CommandSender sender, Command command, String label, String[] args) {
+            this.sender = sender;
+            this.command = command;
+            this.label = label;
+            this.args = args;
+        }
+
+        public CommandSender sender() {
+            return sender;
+        }
+
+        public Command command() {
+            return command;
+        }
+
+        public String label() {
+            return label;
+        }
+
+        public String[] args() {
+            return args;
+        }
 
     }
 }
